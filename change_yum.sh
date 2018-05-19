725a97e2d68d185bd7e22a27902309cbe1e6f036
qishiyexu <qishiyexu@126.com>
2018-05-19 16:46:31 +0000

    .

--- change_yum.sh
+++ change_yum.sh
@@ -1,3 +1,3 @@
 mv /etc/yum.repos.d/CentOS-Base.repo{,.bak}
 wget -O /etc/yum.repos.d/CentOS-Base.repo \
-http://mirrors.aliyun.com/repo/Centos-7.repo
+http://mirrors.163.com/.help/CentOS7-Base-163.repo
