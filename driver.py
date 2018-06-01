54e0d374065578b38a71354afd096f620f597cc1
LAPTOP-9H549B88\chen <85604775@qq.com>
2018-06-01 16:15:07 +0800

    no message

--- driver.py
+++ driver.py
@@ -5415,13 +5415,13 @@ class LibvirtDriver(driver.ComputeDriver):
 </append>
 """
 
-#         hack_allow_mc_xml = """
-# <domain>
-# <qemu:commandline>
-#     <qemu:env name='SPICE_DEBUG_ALLOW_MC' value='1'/>
-# </qemu:commandline>
-# </domain>
-# """
+        hack_allow_mc_xml = """
+<domain>
+<qemu:commandline>
+    <qemu:env name='SPICE_DEBUG_ALLOW_MC' value='1'/>
+</qemu:commandline>
+</domain>
+"""
 
         libvit_obj = xml.dom.minidom.parseString(pre_xml)
         hack_obj = xml.dom.minidom.parseString(hack_xml)
@@ -5429,9 +5429,11 @@ class LibvirtDriver(driver.ComputeDriver):
             if (isinstance(c_lib_obj, xml.dom.minidom.Element) and c_lib_obj.tagName == 'devices'):
                 c_lib_obj.childNodes.extend(hack_obj.childNodes[0].childNodes)
 
-        # hack_allow_mc_obj = xml.dom.minidom.parseString(hack_allow_mc_xml)
-        # domain_e = libvit_obj.getElementsByTagName('domain')
-        # domain_e.appendChild(hack_allow_mc_obj)
+        hack_allow_mc_obj = xml.dom.minidom.parseString(hack_allow_mc_xml)
+        LOG.error('-----------')
+        LOG.error(hack_allow_mc_obj)
+        domain_e = libvit_obj.getElementsByTagName('domain')
+        domain_e.appendChild(hack_allow_mc_obj)
 
         new_xml = libvit_obj.toxml()
 
